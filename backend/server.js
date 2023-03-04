ada50222ae44bdcd757b2bebf8870bbee53bda1c
Stone Comstock <comstockstone@yahoo.com>
2023-03-04 21:39:11 +0700

    db connection setup

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
 // const { errorHandler } = require('./middleware/errorMiddleware');
-// const connectDB = require('./config/db');
+const connectDB = require('./config/db');
 const PORT = process.env.PORT || 8000;
 
 // Connect to DB
-// connectDB();
+connectDB();
 
 const app = express();
 app.use(express.json());
