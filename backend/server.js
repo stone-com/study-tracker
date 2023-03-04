76ee4e1268f81c6929d16e1c3f68ec46839650e4
Stone Comstock <comstockstone@yahoo.com>
2023-03-04 21:26:19 +0700

    Basic server file setup

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+// const { errorHandler } = require('./middleware/errorMiddleware');
+// const connectDB = require('./config/db');
+const PORT = process.env.PORT || 8000;
+
+// Connect to DB
+// connectDB();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the expense tracker!' });
+});
+
+// Routes
+
+// app.use(errorHandler);
+
+app.listen(PORT, () => console.log('Server started on port ' + PORT));
